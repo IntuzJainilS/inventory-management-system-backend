17742c84f906a2820cc2592a072c319bc1ca6c0a
IntuzJainilSalot <jainils@intuz.net>
2026-04-08 13:08:29 +0530

    added searching and pagination in get product route

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,19 +1,46 @@
 import { Request, Response } from "express";
 import { Products } from "../models/ProductModel";
+import { Op } from "sequelize";
 
 export const getProducts = async (req: Request, res: Response) => {
     try {
-        const products = await Products.findAll();
-        if (products.length == 0) {
-            return res.status(400).json({
-                success:false,
-                message:"No products found"
-            })
+        const {
+            page = 1,
+            limit = 4,
+            search,
+        } = req.query;
+
+        const offset = (Number(page) - 1) * Number(limit);
+
+        const whereClause: any = {};
+
+        if (search) {
+            whereClause[Op.or] = [
+                { name: { [Op.like]: `%${search}%` } },
+            ];
         }
+        const {count, rows} = await Products.findAndCountAll({where: whereClause, limit: Number(limit), offset: offset,});
+        if (count === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No user found",
+            });
+        }
+        // if (products.length == 0) {
+        //     return res.status(400).json({
+        //         success: false,
+        //         message: "No products found"
+        //     })
+        // }
         return res.status(200).json({
             success: true,
             message: "Products fetched successfully",
-            data: products
+            data: rows,
+            pagination: {
+                totalItems: count,
+                totalPages: Math.ceil(count / Number(limit)),
+                currentPage: Number(page)
+            }
         })
     } catch (error) {
         return res.status(404).json({
